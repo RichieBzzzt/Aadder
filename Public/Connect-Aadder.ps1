5015bdcdf01c6518ef5c676d92794ffe2cd0cd35
Richie Lee <richie@bzzzt.io>
2020-10-29 15:43:22 +0000

    update function

--- Public/Connect-Aadder.ps1
+++ Public/Connect-Aadder.ps1
@@ -1,18 +1,35 @@
 Function Connect-Aadder {
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(Mandatory = $false)]        
         [switch]
-        $InstallAzureAD
+        $InstallAzureAD,
+        [Parameter(Mandatory = $false, ParameterSetName = 'graph')]  
+        [switch]
+        $graph
     )
     if ($PSBoundParameters.ContainsKey('InstallAzureAD') -eq $true) {
         Install-Module AzureAD -Scope CurrentUser -AllowClobber -Force -Verbose
     }
     Import-Module AzureAD -Force
+
     $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
-    $graphToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.microsoft.com").AccessToken
+
+    $ConnectAzureAd = @{
+        AccountId = $context.Account.Id
+        TenantId  = $context.Tenant.Id
+    }
+
     $aadToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.windows.net").AccessToken
-    Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.tenant.id -MsAccessToken $graphToken -Verbose
+    $ConnectAzureAd.Add("AadAccessToken", $aadToken)
+
+    if ($PSBoundParameters.ContainsKey('graph') -eq $true) {
+        Write-Host "Adding Graph Token"
+        $graphToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.microsoft.com").AccessToken
+        $ConnectAzureAd.Add("MsAccessToken", $graphToken)
+    }
+
+    Connect-AzureAD @ConnectAzureAd -Verbose
 }
 
 
